510ad2515a0a168528566e0074e428f507490829
Bruno FS <bruno.serbena@gmail.com>
2019-07-25 01:14:55 -0300

    unit test for board initialization

--- src/app/tictactoe-board/tictactoe-board.component.spec.ts
+++ src/app/tictactoe-board/tictactoe-board.component.spec.ts
@@ -22,4 +22,13 @@ describe('TictactoeBoardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should be all empty at the start', () => {
+    for (let rowNo = 0; rowNo < component.boardArray.length; rowNo++) {
+      for (let colNo = 0; colNo < component.boardArray[rowNo].length; colNo++) {
+        expect(component.isEmpty(rowNo, colNo)).toBeTruthy();
+      }
+    }
+  })
 });
