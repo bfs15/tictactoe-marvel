2e85a9de8dfccbbff562c51ff33b81e588943090
Bruno FS <bruno.serbena@gmail.com>
2019-07-25 22:37:37 -0300

    game fixed test

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -1,6 +1,8 @@
+import { GameScoreComponent } from './../game-score/game-score.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameComponent } from './game.component';
+import { TictactoeBoardComponent } from '../tictactoe-board/tictactoe-board.component';
 
 describe('GameComponent', () => {
   let component: GameComponent;
@@ -8,7 +10,10 @@ describe('GameComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
+      declarations: [
+        GameComponent,
+        GameScoreComponent,
+        TictactoeBoardComponent, ]
     })
     .compileComponents();
   }));
