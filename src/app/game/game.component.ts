25cc208aab4857ae20653b47e9a10f9151383c30
Bruno FS <bruno.serbena@gmail.com>
2019-07-26 01:33:30 -0300

    increase score on win & bound players to gameScore

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Player } from './../player';
 import { Component, OnInit } from '@angular/core';
+import { EndgameStatus, EndgameType } from '../board';
 
 @Component({
   selector: 'app-game',
@@ -10,17 +11,23 @@ export class GameComponent implements OnInit {
   players: Player[] = undefined;
   private isBoardEndgame_: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
-  onEndgameEvent(event){
-    // TODO: add score to winning player
+  ngOnInit() {
+    // TODO: implement character seletion
+    this.players = [new Player("Spider-Woman", "http://i.annihil.us/u/prod/marvel/i/mg/a/10/528d369de3e4f.jpg"), new Player("Thanos")];
+  }
+
+  onEndgameEvent(event: EndgameStatus){
+    if (event.type == EndgameType.win){
+      this.players[event.playerId].score ++;
+    }
     this.isBoardEndgame_ = true;
   }
 
   onResetBoard(event, boardComponent) {
+    // TODO: choose random first player
     this.isBoardEndgame_ = false;
     boardComponent.newBoard();
   }
