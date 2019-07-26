9ddb4d30534031402e1bc577155368c3f4e7d1c8
Bruno FS <bruno.serbena@gmail.com>
2019-07-26 18:24:27 -0300

    players turn chosen randomly

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,8 +8,13 @@ import { EndgameStatus, EndgameType } from '../board';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+  // list of players, undefined until selected by users
   players: Player[] = undefined;
-  private isBoardEndgame_: boolean = false;
+  // whether board is at endgame state
+  public isBoardEndgame_: boolean = false;
+  // list of players' Indexes by turn order
+  // [x] = y means player[y] goes at turn x
+  private turnOrder_: number[];
 
   constructor() {
   }
@@ -17,17 +22,63 @@ export class GameComponent implements OnInit {
   ngOnInit() {
     // TODO: implement character seletion
     this.players = [new Player("Spider-Woman", "http://i.annihil.us/u/prod/marvel/i/mg/a/10/528d369de3e4f.jpg"), new Player("Thanos")];
+
+    this.turnOrder_ = new Array<number>(this.players.length).fill(-1);
+    this.chooseTurnOrder();
   }
 
+  // return player index from his turn order
+  // id is the turn order the player goes at (playerId 0 goes first)
+  indexFromTurnNo(id: number): number{
+    return this.turnOrder_[id];
+  }
+
+  // add to winning players score
   onEndgameEvent(event: EndgameStatus){
-    if (event.type == EndgameType.win){
-      this.players[event.playerId].score ++;
+    // endgameStatus has info about how the game ended
+    if (event.type == EndgameType.win) {
+      // winning player id (turn order) is event.playerId
+      // get player index from his turn order
+      let playerWinIndex = this.indexFromTurnNo(event.playerId);
+      this.players[playerWinIndex].score ++;
     }
     this.isBoardEndgame_ = true;
   }
 
+  // updates player turns according to this.turnOrder_
+  updatePlayerTurns(){
+    for (let turnNo = 0; turnNo < this.turnOrder_.length; turnNo++) {
+      this.players[this.indexFromTurnNo(turnNo)].turn = turnNo;      
+    }
+  }
+
+  // randomly assigns player turn order
+  chooseTurnOrder(){
+    // make turn order list (list player indexes in turn order)
+    // equal chance of choosing any index
+    let turnOrder = new Array<number>(this.turnOrder_.length);
+    let firstPlayerIndex = Math.floor(Math.random() * this.players.length);
+    turnOrder[0] = firstPlayerIndex;
+    if (turnOrder[0] == 0) {
+      turnOrder[1] = 1;
+    } else {
+      turnOrder[1] = 0;
+    }
+    // set turn order made
+    this.setTurnOrder(turnOrder);
+  }
+
+  setTurnOrder(turnOrder: number[]) {
+    this.turnOrder_ = turnOrder;
+    // updates player turn numbers as well
+    this.updatePlayerTurns();
+  }
+
+  // starts new match on the board
   onResetBoard(event, boardComponent) {
-    // TODO: choose random first player
+    // chooses new player order
+    this.chooseTurnOrder();
+    // resets board
     this.isBoardEndgame_ = false;
     boardComponent.newBoard();
   }
