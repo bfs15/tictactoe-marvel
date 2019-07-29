b943e024f1ef82fe74404465171d76d4baa92b01
Bruno FS <bruno.serbena@gmail.com>
2019-07-29 01:48:07 -0300

    updated app test to render gameComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,10 +32,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render game', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toBeTruthy();
+    expect(compiled.querySelector('app-game').textContent).toBeTruthy();
   });
 });
